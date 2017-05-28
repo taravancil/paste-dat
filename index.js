f3a17c8d446a524dc340a18d5b4d50e9aeddd494
Tara Vancil <tbvanc@gmail.com>
2017-05-27 22:37:16 -0500

    Update form

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mainEl = document.querySelector('main')
+const formsEl = document.getElementById('forms-container')
 const submitBtn = document.getElementById('submit')
 let messageEl = document.getElementById('message')
 let formCount = 0 // the number of file forms that are currently rendered
@@ -10,29 +10,27 @@ submitBtn.addEventListener('click', createGist)
 document.getElementById('add-file').addEventListener('click', appendForm)
 
 function createGist () {
-  // Create a new archive
-  // Get the title/desc if one exists
   var archive
-
   DatArchive.create({
     title: document.querySelector('input[name="title"]').value || '',
     description: document.querySelector('input[name="description"]').value || ''
-  }).then(function (res) {
-    archive = res
+  })
+    .then(function (res) {
+      archive = res
 
-    // Figure out which files need to be added
-    var fileForms = document.querySelectorAll('form')
-    var promises = []
+      // Figure out which files need to be added
+      var fileForms = document.querySelectorAll('form')
+      var promises = []
 
-    for (var i = 0; i < fileForms.length; i++) {
-      var form = fileForms[i]
-      var path = form.path.value
-      var content = form.content.value
+      for (var i = 0; i < fileForms.length; i++) {
+        var form = fileForms[i]
+        var path = form.path.value
+        var content = form.content.value
 
-      if (path && content) {
-        promises.push(archive.writeFile(path, content))
+        if (path && content) {
+          promises.push(archive.writeFile(path, content))
+        }
       }
-    }
 
       Promise.all(promises)
         .then(function (data) {
@@ -51,10 +49,8 @@ function appendForm () {
   form.id = 'add-file-form-' + formCount
 
   const formContent = `
-    <label for="title">Title</label>
-    <input name="title" placeholder="Title"/>
     <!-- TODO allow user to do any kind of file -->
-    <input name="path" placeholder="Filename"/>
+    <input autofocus name="path" placeholder="Filename including extension"/>
     <textarea name="content"></textarea>
   `
 
