65f22880f8db74a1b4a8caf9ea2e7417497646ab
Tara Vancil <tbvanc@gmail.com>
2017-05-27 22:20:57 -0500

    Add functionality to create a gist

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 const mainEl = document.querySelector('main')
+const submitBtn = document.getElementById('submit')
 let formCount = 0 // the number of file forms that are currently rendered
 
 // render one form by default
 appendForm()
 
+submitBtn.addEventListener('click', createGist)
 document.getElementById('add-file').addEventListener('click', appendForm)
+
+function createGist () {
+  // Create a new archive
+  // Get the title/desc if one exists
+  var archive
+
+  DatArchive.create({
+    title: document.querySelector('input[name="title"]').value || '',
+    description: document.querySelector('input[name="description"]').value || ''
+  }).then(function (res) {
+    archive = res
+
+    // Figure out which files need to be added
+    var fileForms = document.querySelectorAll('form')
+    var promises = []
+
+    for (var i = 0; i < fileForms.length; i++) {
+      var form = fileForms[i]
+      var path = form.path.value
+      var content = form.content.value
+
+      if (path && content) {
+        promises.push(archive.writeFile(path, content))
+      }
+    }
+
+    Promise.all(promises)
+      .then(function (data) {
+        archive.commit()
+      })
+  })
+}
 
 function appendForm () {
   formCount += 1
@@ -16,7 +50,7 @@ function appendForm () {
     <input name="title" placeholder="Title"/>
     <!-- TODO allow user to do any kind of file -->
     <input name="path" placeholder="Filename"/>
-    <textarea></textarea>
+    <textarea name="content"></textarea>
   `
 
   form.innerHTML = formContent
